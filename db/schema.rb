157fd162aece58c5360b344aead45bf3bbccc02d
j2331-create <bz4.8sgw@gmail.com>
2026-09-01 13:39:41 +0900

    イントロダクションの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_31_024316) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_01_042605) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -68,6 +68,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_31_024316) do
     t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.string "email"
+    t.text "introduction"
     t.string "name"
     t.string "password_digest"
     t.string "remember_digest"
